7e2a5e65a06c726802ff48c9df1405949de27db0
AkakiyBerula <vlad5dyakun.gmail.com>
2024-02-14 19:17:40 +0200

    Додайте за допомогою пакетного менеджеру бібліотеку Axios до проєкту. Сформуйте в окремому файлі инстанс Axios, сконфігуруйте його та імпортуйте за потребою.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    
-  </main>
+  <div>
+    <HelloWorld :msg="'Hello!!!'"/>
+    <AxiosAPI />
+  </div>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import AxiosAPI from './components/AxiosAPI.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    AxiosAPI
+  }
+}
+</script>
 
 <style scoped>
 header {
---
+++ src/components/AxiosAPI.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-container>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+import axiosInstance from "../../services/axios";
+
+
+export default {
+  name: 'AxiosAPI',
+  data() {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    getProductsAPI() {
+      axiosInstance.get("/products")
+        .then(response => {
+          this.products = response.data
+          console.log(this.products)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  mounted() {
+    this.getProductsAPI();
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
